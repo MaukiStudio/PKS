1a496511374aa81e8f2dc5a83bac22c4760210ec
gulby <gulby@maukistudio.com>
2016-02-15 19:13:28 +0900

    구조 세팅을 위한 Test 추가

--- pks/account/tests.py
+++ pks/account/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from account.models import VirtualDevice
+
+
+class ApiRootTest(APITestCase):
+    def test_can_connect(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
