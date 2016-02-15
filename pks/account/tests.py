36940d573e8105afe4ccc30acddc7893ebb156df
gulby <gulby@maukistudio.com>
2016-02-15 23:54:15 +0900

    api root 테스트 추가

--- pks/account/tests.py
+++ pks/account/tests.py
@@ -1,5 +1,8 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
+
+import json
 
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -12,3 +15,8 @@ class ApiRootTest(APITestCase):
 
     def test_can_connect(self):
         self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+
+    def test_valid_json(self):
+        decoded = json.JSONDecoder().decode(self.response.content)
+        encoded = json.JSONEncoder(separators=(',', ':')).encode(decoded)
+        self.assertEqual(encoded, self.response.content)
