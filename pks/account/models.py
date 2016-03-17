4735916f4626d3191589fb62be4794787c162fac
gulby <gulby@maukistudio.com>
2016-03-17 18:48:56 +0900

    RealUser model 추가

--- pks/account/models.py
+++ pks/account/models.py
@@ -6,9 +6,13 @@ from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
 
 
+class RealUser(models.Model):
+    email = models.EmailField(blank=True, null=True, default=None)
+
+
 class VD(models.Model):
     owner = models.ForeignKey(User, on_delete=models.SET_DEFAULT, null=True, default=None)
-    last_lonlat = models.PointField(blank=True, null=True, default=None)
+    lastLonLat = models.PointField(blank=True, null=True, default=None)
     deviceName = models.CharField(max_length=36, blank=True, null=True, default=None)
     deviceTypeName = models.CharField(max_length=36, blank=True, null=True, default=None)
     data = JSONField(blank=True, null=True, default=None)
--- pks/account/test_models.py
+++ pks/account/test_models.py
@@ -19,43 +19,66 @@ class VDTest(TestCase):
         self.user.password = SG('[\l]{6:10}&[\d]{2}').render()
         self.user.save()
 
-        self.vd = models.VD()
-        self.vd.owner = self.user
-
     def test_save_and_retreive(self):
-        self.vd.save()
+        vd = models.VD()
+        vd.save()
         saved = models.VD.objects.first()
-        self.assertEqual(saved, self.vd)
+        self.assertEqual(saved, vd)
+
+    def test_owner_property(self):
+        vd = models.VD(owner=self.user)
+        vd.save()
+        saved = models.VD.objects.first()
         self.assertEqual(saved.owner, self.user)
 
-    def test_can_save_same_owner(self):
-        self.vd.save()
         vd2 = models.VD()
-        vd2.owner = self.vd.owner
+        vd2.owner = vd.owner
         vd2.save()
-        self.assertNotEqual(self.vd, vd2)
-        self.assertEqual(self.vd.owner, vd2.owner)
+        self.assertNotEqual(vd, vd2)
+        self.assertEqual(vd.owner, vd2.owner)
+
+    def test_owner_relationship(self):
+        self.assertEqual(self.user.vd_set.all().count(), 0)
+        vd = models.VD(owner=self.user)
+        vd.save()
+        self.assertEqual(self.user.vd_set.all().count(), 1)
+        vd2 = models.VD(owner=self.user)
+        vd2.save()
+        self.assertEqual(self.user.vd_set.all().count(), 2)
 
     def test_simple_properties(self):
         deviceName = SG('[\w\-]{36}').render()
         deviceTypeName = 'LG-F460L'
-        self.vd.deviceName = deviceName
-        self.vd.deviceTypeName = deviceTypeName
-        self.vd.save()
+        vd = models.VD()
+        vd.deviceName = deviceName
+        vd.deviceTypeName = deviceTypeName
+        vd.save()
         saved = models.VD.objects.first()
         self.assertEqual(saved.deviceName, deviceName)
         self.assertEqual(saved.deviceTypeName, deviceTypeName)
 
-    def test_json_properties(self):
+    def test_data_property(self):
         j = '{"deviceName": "%s", "deviceTypeName": "%s"}' % (SG('[\w\-]{36}').render(), 'LG-F460L')
-        self.vd.data = json.loads(j)
-        self.vd.save()
+        vd = models.VD()
+        vd.data = json.loads(j)
+        vd.save()
         saved = models.VD.objects.first()
         self.assertEqual(j, json.dumps(saved.data, encoding='utf-8'))
 
-    def test_gis_properties(self):
+    def test_lastLonLat_property(self):
         point = GEOSGeometry('POINT(127.0850802 37.4005048)')
-        self.vd.last_lonlat = point
-        self.vd.save()
+        vd = models.VD()
+        vd.lastLonLat = point
+        vd.save()
         saved = models.VD.objects.first()
-        self.assertEqual(point, saved.last_lonlat)
+        self.assertEqual(point, saved.lastLonLat)
+
+
+class RealUserTest(TestCase):
+
+    def test_save_and_retreive(self):
+        real_user = models.RealUser()
+        real_user.save()
+        saved = models.RealUser.objects.first()
+        self.assertEqual(saved, real_user)
+
