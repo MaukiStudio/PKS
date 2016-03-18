3a75fb2f7b5de778eb92fbba712d76cefbaad0c0
gulby <gulby@maukistudio.com>
2016-03-18 18:45:29 +0900

    HyperlinkedModelSerializer 로 변경

--- pks/account/serializers.py
+++ pks/account/serializers.py
@@ -2,29 +2,29 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
 
 from account import models
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
+        fields = ('url', 'username', 'password', 'email')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(username=validated_data['username'])
+        user = User(username=validated_data['username'], email=validated_data['email'])
         user.set_password(validated_data['password'])
         user.save()
         return user
 
 
-class RealUserSerializer(ModelSerializer):
+class RealUserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = models.RealUser
 
 
-class VDSerializer(ModelSerializer):
+class VDSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = models.VD
