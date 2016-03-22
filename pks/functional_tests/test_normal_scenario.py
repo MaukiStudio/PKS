70082679311984bd6afe98c2affeb293a1c651f1
gulby <gulby@maukistudio.com>
2016-03-22 23:38:01 +0900

    FT 보강 및 TODO 수정

--- pks/functional_tests/test_normal_scenario.py
+++ pks/functional_tests/test_normal_scenario.py
@@ -13,39 +13,39 @@ class FirstScenarioTest(APITestBase):
 
     def test_first_scenario(self):
         # 자동 회원 가입
-        res1 = self.client.post('/users/register/')
-        self.assertEqual(res1.status_code, status.HTTP_201_CREATED)
+        response = self.client.post('/users/register/')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # 유저 로그인 by 토큰
-        auth_user_token = json.loads(res1.content)['auth_user_token']   # post 와는 다른, 최대한 안전한 storage 에 저장
+        auth_user_token = json.loads(response.content)['auth_user_token']   # post 와는 다른, 최대한 안전한 storage 에 저장
         self.assertNotLogin()
-        res2 = self.client.post('/users/login/', dict(auth_user_token=auth_user_token))
-        self.assertEqual(res2.status_code, status.HTTP_302_FOUND)
+        response = self.client.post('/users/login/', dict(auth_user_token=auth_user_token))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
         self.assertLogin()
 
         # VD 등록
-        res3 = self.client.post('/vds/register/',
+        response = self.client.post('/vds/register/',
                                 dict(email='gulby@maukistudio.com',     # required
                                      deviceTypeName='LG-F460L',         # optional
                                      deviceName='ed750c68-ecd1-11e5-b311-382c4a6424bd'))    # optional
-        self.assertEqual(res3.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         vd = models.VD.objects.first()
 
         # VD 로그인 by 토큰
-        auth_vd_token = json.loads(res3.content)['auth_vd_token']       # post 와 같은 storage 에 저장해도 무방
+        auth_vd_token = json.loads(response.content)['auth_vd_token']       # post 와 같은 storage 에 저장해도 무방
         self.assertVdNotLogin()
-        res5 = self.client.post('/vds/login/', dict(auth_vd_token=auth_vd_token))
-        self.assertEqual(res5.status_code, status.HTTP_302_FOUND)
+        response = self.client.post('/vds/login/', dict(auth_vd_token=auth_vd_token))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
         self.assertVdLogin()
-        auth_vd_token = json.loads(res5.content)['auth_vd_token']       # 로그인할 때마다 auth_vd_token 은 갱신
+        auth_vd_token = json.loads(response.content)['auth_vd_token']       # 로그인할 때마다 auth_vd_token 은 갱신
 
         # RealUser(VD.realOwner) 에 매핑된 VD 목록 조회
-        res6 = self.client.get('/rus/mine/vds/')
+        response = self.client.get('/rus/mine/vds/')
 
         # 이메일 인증이 되지 않은 경우 404 Not Found 가 발생
         # TODO : 현재는 임시로 VD 등록 시 이메일 인증도 완료된 걸로 처리되어 있음
         # TODO : 따라서 404 가 발생하지 않음. 추후 주석 제거
-        #self.assertEqual(res6.status_code, status.HTTP_404_NOT_FOUND)
+        #self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
         # 앱 종료
         self.logout()
@@ -58,26 +58,48 @@ class FirstScenarioTest(APITestBase):
 
         # 유저 로그인 by 토큰
         self.assertNotLogin()
-        res7 = self.client.post('/users/login/', dict(auth_user_token=auth_user_token))
-        self.assertEqual(res7.status_code, status.HTTP_302_FOUND)
+        response = self.client.post('/users/login/', dict(auth_user_token=auth_user_token))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
         self.assertLogin()
 
         # VD 로그인 by 토큰
         self.assertVdNotLogin()
-        res8 = self.client.post('/vds/login/', dict(auth_vd_token=auth_vd_token))
-        self.assertEqual(res8.status_code, status.HTTP_302_FOUND)
+        response = self.client.post('/vds/login/', dict(auth_vd_token=auth_vd_token))
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
         self.assertVdLogin()
 
         # RealUser(VD.realOwner) 에 매핑된 VD 목록 조회
-        res9 = self.client.get('/rus/mine/vds/')
-        self.assertEqual(res9.status_code, status.HTTP_200_OK)
+        response = self.client.get('/rus/mine/vds/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         # 하나도 없음을 확인 : 자기자신은 포함되지 않으므로 하나도 없음
-        vds = json.loads(res9.content)
+        vds = json.loads(response.content)
         self.assertEqual(len(vds), 0)
+
+        # 새로운 단말기에서 같은 RealUser 로...
+        self.logout()
+        response = self.client.post('/users/register/')
+        auth_user_token_other = json.loads(response.content)['auth_user_token']
+        response = self.client.post('/users/login/', dict(auth_user_token=auth_user_token_other))
+        response = self.client.post('/vds/register/', dict(email='gulby@maukistudio.com'))
+        auth_vd_token_other = json.loads(response.content)['auth_vd_token']
+        response = self.client.post('/vds/login/', dict(auth_vd_token=auth_vd_token_other))
+        self.assertLogin()
+        self.assertVdLogin()
+
+        # RealUser(VD.realOwner) 에 매핑된 VD 목록 조회
+        response = self.client.get('/rus/mine/vds/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # 앞서 생성되어 있던 VD 1개가 존재함을 확인
+        vds = json.loads(response.content)
+        self.assertEqual(len(vds), 1)
+        self.assertEqual(vd.id, vds[0]['id'])
+
+
+
+
 
         # 장소 저장
         self.fail('장소 저장 로직 구현')
 
-        # 새로운 단말기에서 같은 RealUser 로 진행하는 시나리오...
-        self.fail('새로운 단말기 시나리오...')
