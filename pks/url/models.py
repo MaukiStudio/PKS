f9caa0524abde25996cda5ffd0902da48e9e0cc3
gulby <gulby@maukistudio.com>
2016-03-29 15:24:46 +0900

    Url 모델 구현

--- pks/url/models.py
+++ pks/url/models.py
@@ -1,8 +1,32 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from hashlib import md5
+from uuid import UUID
+from base64 import b16encode
+from django.utils import timezone
 from django.contrib.gis.db import models
 
 
 class Url(models.Model):
-    pass
+    uuid = models.UUIDField(primary_key=True, default=None)
+    url = models.URLField(blank=True, null=True, default=None)
+    content = models.TextField(blank=True, null=True, default=None)
+    lastCrawlDate = models.DateTimeField(blank=True, null=True, default=None)
+
+    def set_uuid(self):
+        m = md5()
+        m.update(self.url)
+        h = m.digest()
+        self.uuid = UUID(b16encode(h))
+        return self.uuid
+
+    def save(self, *args, **kwargs):
+        if self.url and not self.uuid:
+            self.set_uuid()
+        if self.content:
+            self.lastCrawlDate = timezone.now()
+        super(Url, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.url
--- pks/url/test_models.py
+++ pks/url/test_models.py
@@ -2,13 +2,65 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+from django.utils import timezone
+from datetime import timedelta
+from uuid import uuid1
+from django.db import IntegrityError
+
 from base.tests import APITestBase
+from url import models
 
 
 class UrlTest(APITestBase):
 
     def test_string_representation(self):
-        self.fail()
+        url = models.Url()
+        url.url = 'http://www.maukistudio.com/'
+        self.assertEqual(str(url), url.url)
 
     def test_save_and_retreive(self):
-        self.fail()
+        url = models.Url()
+        url.uuid = uuid1()
+        url.save()
+        saved = models.Url.objects.first()
+        self.assertEqual(saved, url)
+        self.assertEqual(saved.uuid, url.uuid)
+
+    def test_url_property(self):
+        url = models.Url()
+        test_value = 'http://www.maukistudio.com/'
+        url.url = test_value
+        url.save()
+        saved = models.Url.objects.first()
+        self.assertEqual(saved.url, test_value)
+
+        url2 = models.Url()
+        url2.url = url.url
+        self.assertEqual(1, models.Url.objects.count())
+        url.save()
+        self.assertEqual(1, models.Url.objects.count())
+
+        with self.assertRaises(IntegrityError):
+            models.Url.objects.create(url=url.url)
+
+    def test_content_property(self):
+        url = models.Url(url='http://www.maukistudio.com/')
+        test_value = '<html>maukistudio</html>'
+        url.content = test_value
+        url.save()
+        saved = models.Url.objects.first()
+        self.assertEqual(saved.content, test_value)
+
+    def test_lastAccessDate_property(self):
+        url = models.Url(url='http://www.maukistudio.com/')
+        url.save()
+        saved = models.Url.objects.first()
+        self.assertEqual(None, url.lastCrawlDate)
+        self.assertEqual(None, saved.lastCrawlDate)
+
+        url.content = 'content'
+        url.save()
+        saved = models.Url.objects.first()
+        self.assertIsNotNone(url.lastCrawlDate)
+        self.assertAlmostEqual(timezone.now(), url.lastCrawlDate, delta=timedelta(seconds=10))
+        self.assertEqual(saved.lastCrawlDate, url.lastCrawlDate)
