a3263bd79cf56da9258b08f99d67aec842afc502
gulby <gulby@maukistudio.com>
2016-04-01 18:12:23 +0900

    PlaceContent 설계 변경에 따른 Place.post 구현 수정

--- pks/place/models.py
+++ pks/place/models.py
@@ -11,42 +11,61 @@ from url.models import Url
 from content.models import FsVenue, ShortText
 from delorean import Delorean
 
+# stext_type
+STEXT_TYPE_PLACE_NOTE = 1
+STEXT_TYPE_PLACE_NAME = 2
+STEXT_TYPE_ADDRESS = 3
+STEXT_TYPE_IMAGE_NOTE = 4
+
 
 class Place(models.Model):
-    post_cache = None
-
     @property
     def post(self):
-        if self.post_cache:
-            return self.post_cache
-        result = dict(id=self.id, latitude=None, longitude=None, images=list(), urls=list(), fsVenue=None, notes=list(), name=None, addr=None,)
+        result = dict(id=self.id, lonLat=None, images=list(), urls=list(), fsVenue=None, notes=list(), name=None, addr=None,)
         for pc in self.pcs.all().order_by('-uuid'):
-            if pc.lonLat and not result['latitude']: result['latitude'] = pc.lonLat.y
-            if pc.lonLat and not result['longitude']: result['longitude'] = pc.lonLat.x
-            if pc.image: result['images'].append(pc.image.file.url)
-            if pc.url: result['urls'].append(pc.url.url)
-            if pc.fsVenue and not result['fsVenue']: result['fsVenue'] = pc.fsVenue.fsVenueId
-            if pc.note: result['notes'].append(pc.note.content)
-            if pc.name and not result['name']: result['name'] = pc.name.content
-            if pc.addr and not result['addr']: result['addr'] = pc.addr.content
-        self.post_cache = result
-        return self.post_cache
+            if pc.lonLat and not result['lonLat']:
+                result['lonLat'] = dict(lon=pc.lonLat.x, lat=pc.lonLat.y)
+
+            if pc.image:
+                v = str(pc.image)
+                if v not in result['images']:
+                    result['images'].append(v)
+
+            if pc.url:
+                v = pc.url.url
+                if v not in result['urls']:
+                    result['urls'].append(v)
+
+            if pc.fsVenue and not result['fsVenue']:
+                result['fsVenue'] = pc.fsVenue.fsVenueId
+
+            if pc.stext:
+                if pc.stext_type == STEXT_TYPE_PLACE_NOTE:
+                    result['notes'].append(pc.stext.content)
+                if pc.stext_type == STEXT_TYPE_PLACE_NAME:
+                    if not result['name']:
+                        result['name'] = pc.stext.content
+                if pc.stext_type == STEXT_TYPE_ADDRESS:
+                    if not result['addr']:
+                        result['addr'] = pc.stext.content
+        return result
 
 
 class PlaceContent(models.Model):
     # uuid
     uuid = models.UUIDField(primary_key=True, default=None)
 
-    # References
+    # node
     place = models.ForeignKey(Place, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
     vd = models.ForeignKey(VD, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
-
-    # Contents
-    lonLat = models.PointField(blank=True, null=True, default=None)
     image = models.ForeignKey(Image, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
     url = models.ForeignKey(Url, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
     fsVenue = models.ForeignKey(FsVenue, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
     stext = models.ForeignKey(ShortText, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='pcs')
+
+    # value
+    lonLat = models.PointField(blank=True, null=True, default=None)
+    stext_type = models.SmallIntegerField(blank=True, null=True, default=None)
 
     def set_uuid(self):
         timestamp = int(round(Delorean().epoch*1000))
--- pks/place/test_models.py
+++ pks/place/test_models.py
@@ -25,47 +25,62 @@ class PlaceTest(APITestBase):
         saved = models.Place.objects.first()
         self.assertEqual(saved, place)
 
-    def __skip__test_post(self):
+    def test_post(self):
         place = models.Place()
         place.save()
 
         vd1 = VD(); vd1.save()
-        name1 = Name(content='능라'); name1.save()
-        addr1 = Address(content='경기도 성남시 분당구 운중동 883-3'); addr1.save()
-        note11 = Note(content='분당 냉면 최고'); note11.save()
-        note12 = Note(content='만두도 괜찮음'); note12.save()
+        point1 = GEOSGeometry('POINT(127 37)')
+        name1 = ShortText(content='능라'); name1.save()
+        addr1 = ShortText(content='경기도 성남시 분당구 운중동 883-3'); addr1.save()
+        note11 = ShortText(content='분당 냉면 최고'); note11.save()
+        note12 = ShortText(content='만두도 괜찮음'); note12.save()
         img1 = Image(file=self.uploadImage('test.jpg')); img1.save()
-        url1 = Url(url='http://maukistudio.com/'); url1.save()
-        pc11 = models.PlaceContent(vd=vd1, place=place, name=name1, addr=addr1, note=note11, image=img1, url=url1); pc11.save()
-        pc12 = models.PlaceContent(vd=vd1, place=place, lonLat=GEOSGeometry('POINT(127 37)'), note=note12); pc12.save()
+
+        # 현재 위치 저장
+        pc11 = models.PlaceContent(vd=vd1, place=place, lonLat=point1, image=img1, stext=addr1, stext_type=models.STEXT_TYPE_ADDRESS); pc11.save()
+        pc12 = models.PlaceContent(vd=vd1, place=place, lonLat=point1, image=img1, stext=note11, stext_type=models.STEXT_TYPE_PLACE_NOTE); pc12.save()
+        # 이름 지정
+        pc13 = models.PlaceContent(vd=vd1, place=place, stext=name1, stext_type=models.STEXT_TYPE_PLACE_NAME); pc13.save()
+        # 노트 추가
+        pc14 = models.PlaceContent(vd=vd1, place=place, stext=note12, stext_type=models.STEXT_TYPE_PLACE_NOTE); pc14.save()
 
         vd2 = VD(); vd2.save()
-        name2 = Name(content='능라도'); name2.save()
-        addr2 = Address(content='경기도 성남시 분당구 산운로32번길 12'); addr2.save()
-        note21 = Note(content='여기 가게 바로 옆으로 이전'); note21.save()
-        note22 = Note(content='평양냉면 맛집'); note22.save()
+        point2 = GEOSGeometry('POINT(127.1037430 37.3997320)')
+        name2 = ShortText(content='능라도'); name2.save()
+        addr2 = ShortText(content='경기도 성남시 분당구 산운로32번길 12'); addr2.save()
+        note21 = ShortText(content='여기 가게 바로 옆으로 이전'); note21.save()
+        note22 = ShortText(content='평양냉면 맛집'); note22.save()
         img2 = Image(file=self.uploadImage('no_exif_test.jpg')); img2.save()
-        url2 = Url(url='http://maukistudio.com/2'); url2.save()
-        pc21 = models.PlaceContent(vd=vd2, place=place, name=name2, addr=addr2, note=note21, image=img2, url=url2); pc21.save()
-        pc22 = models.PlaceContent(vd=vd2, place=place, lonLat=GEOSGeometry('POINT(127.1037430 37.3997320)'), note=note22); pc22.save()
+        url2 = Url(url='http://maukistudio.com/'); url2.save()
 
+        # URL 저장
+        pc21 = models.PlaceContent(vd=vd2, place=place, url=url2, stext=note21, stext_type=models.STEXT_TYPE_PLACE_NOTE); pc21.save()
+        # 이름 지정
+        pc22 = models.PlaceContent(vd=vd2, place=place, stext=name2, stext_type=models.STEXT_TYPE_PLACE_NAME); pc22.save()
+        # 주소 지정
+        pc23 = models.PlaceContent(vd=vd2, place=place, stext=addr2, stext_type=models.STEXT_TYPE_ADDRESS); pc23.save()
+        # 이미지, 노트 추가
+        pc24 = models.PlaceContent(vd=vd2, place=place, lonLat=point2, image=img2, stext=note22, stext_type=models.STEXT_TYPE_PLACE_NOTE); pc24.save()
+        # 장소화
         fsVenue = FsVenue(fsVenueId='40a55d80f964a52020f31ee3'); fsVenue.save()
-        pc13 = models.PlaceContent(vd=vd1, place=place, fsVenue=fsVenue); pc13.save()
+        pc25 = models.PlaceContent(vd=vd1, place=place, fsVenue=fsVenue); pc25.save()
 
         want = json_loads('''
             {
                 "id": %d,
-                "latitude": %f,
-                "longitude": %f,
+                "lonLat": {"lon": %f, "lat": %f},
                 "name": "%s",
                 "addr": "%s",
                 "notes": ["%s", "%s", "%s", "%s"],
                 "images": ["%s", "%s"],
-                "urls": ["%s", "%s"],
+                "urls": ["%s"],
                 "fsVenue": "%s"
             }
-        ''' % (place.id, 37.3997320, 127.1037430, name2, addr2, note22, note21, note12, note11,
-               img2.file.url, img1.file.url, url2, url1, fsVenue,))
+        ''' % (place.id, 127.1037430, 37.3997320, name2, addr2, note22, note21, note12, note11,
+               img2, img1, url2, fsVenue,))
+        print(place.post)
+        print(want)
         self.assertDictEqual(place.post, want)
 
 
@@ -166,8 +181,10 @@ class PlaceContentTest(APITestBase):
     def test_stext_property(self):
         pc = models.PlaceContent()
         pc.stext = self.stext
+        pc.stext_type = models.STEXT_TYPE_PLACE_NOTE
         pc.save()
         saved = self.stext.pcs.get(pk=pc.pk)
         self.assertEqual(pc.stext, self.stext)
         self.assertEqual(saved, pc)
         self.assertEqual(saved.stext, self.stext)
+        self.assertEqual(saved.stext_type, models.STEXT_TYPE_PLACE_NOTE)
