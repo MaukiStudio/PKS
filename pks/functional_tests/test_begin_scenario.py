d126cddf1aac433f364e7875057268625044bb2e
gulby <gulby@maukistudio.com>
2016-04-06 14:54:58 +0900

    페이징 관련 interface spec 정리

--- pks/functional_tests/test_begin_scenario.py
+++ pks/functional_tests/test_begin_scenario.py
@@ -62,8 +62,10 @@ class StartScenarioTest(FunctionalTestAfterLoginBase):
 
         # 내 장소 목록 조회 : userPost 와 placePost 가 한꺼번에 넘어옴
         # 이메일 인증이 되지 않은 시점에서도 ru=myself 는 동작하며, vd=myself 와 동일
-        # 현재까지는 저장한 것이 하나도 없으므로 조회 결과는 없음
-        response = self.client.get('/uplaces/?ru=myself')
+        # limit : 목록 조회시 넘어가는 결과의 개수
+        # offset : 목록 조회를 시작하는 위치 (0에서 시작) (페이지 번호와는 다름)
+        # 10개씩 보여줄 때 20페이지는 limit=10&offset=10*(20-1)=190 임
+        response = self.client.get('/uplaces/?ru=myself&limit=1000&offset=0')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         results = json_loads(response.content)['results']
         self.assertEqual(results, list())
@@ -72,7 +74,6 @@ class StartScenarioTest(FunctionalTestAfterLoginBase):
     def test_places_pos_with_no_place(self):
 
         # 위치 기반 장소 목록 조회
-        # 현재까지는 DB 에 아무것도 없으므로 조회 결과는 없음
         # TODO : 아직 구현되어 있지 않음. 구현해야 함. 일단 /uplaces/?ru=myself 만 사용할 것
 
         # 내장소 목록 조회 : userPost, placePost 둘다 넘어옴
