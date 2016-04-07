7d16f81813e3b754c57509895ad0a4ab1a107e99
gulby <gulby@maukistudio.com>
2016-04-07 23:13:33 +0900

    FT 리팩토링

--- pks/base/tests.py
+++ pks/base/tests.py
@@ -7,7 +7,8 @@ from django.core.files import File
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from rest_framework.test import APITestCase
 from uuid import UUID
-from json import dumps as json_dumps
+from json import loads as json_loads, dumps as json_dumps
+
 
 from pks.settings import VD_SESSION_KEY
 
@@ -62,3 +63,37 @@ class APITestBase(APITestCase):
             json = json_dumps(json)
         print(json)
 
+
+class FunctionalTestBase(APITestBase):
+
+    def __init__(self, *args, **kwargs):
+        self.secureStorage = dict()
+        self.normalStorage = dict()
+        super(FunctionalTestBase, self).__init__(*args, **kwargs)
+
+    def input_from_user(self, default='gulby@maukistudio.com'):
+        return default
+
+    def get_gps_info(self, default=(127.0, 37.0)):
+        return default
+
+    def take_picture(self, default='image/samples/test.jpg'):
+        return default
+
+    def resize_image(self, default='image/samples/test.jpg'):
+        return default
+
+
+class FunctionalTestAfterLoginBase(FunctionalTestBase):
+
+    def setUp(self):
+        response = self.client.post('/users/register/')
+        self.secureStorage['auth_user_token'] = json_loads(response.content)['auth_user_token']
+        response = self.client.post('/users/login/', dict(auth_user_token=self.secureStorage['auth_user_token']))
+        email = self.input_from_user()
+        response = self.client.post('/vds/register/', dict(email=email,))
+        self.normalStorage['auth_vd_token'] = json_loads(response.content)['auth_vd_token']
+        response = self.client.post('/vds/login/', dict(auth_vd_token=self.normalStorage['auth_vd_token']))
+        self.normalStorage['auth_vd_token'] = json_loads(response.content)['auth_vd_token']
+        self.assertLogin()
+        self.assertVdLogin()
