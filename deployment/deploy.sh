baa9363955506cdbf35fe08f097cde0aad72db8c
gulby <gulby@maukistudio.com>
2016-04-08 11:05:53 +0900

    deploy script 정리 및 TODO 갱신

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,7 +2,8 @@ echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" > temp.list
 sudo cp temp.list /etc/apt/sources.list.d/pgdg.list
 rm temp.list
 sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 sudo apt-get install -y python-pip python-dev liblapack3
 sudo apt-get install -y libpq-dev postgresql-9.5 postgresql-contrib-9.5 postgresql-client-9.5
@@ -12,9 +13,8 @@ sudo apt-get install -y python-psycopg2
 sudo easy_install -U pip
 hash -r
 
-sudo pip install -r requirements.txt --upgrade
+sudo -H pip install -r requirements.txt --upgrade
 sudo -u postgres psql -a -f initdb.sql
 
 cd ../pks
 ./notify_model_changed.sh
-
