f0d50f0ed925056e0d04125ea6d114eb6a2b17bf
gulby <gulby@maukistudio.com>
2016-05-12 03:02:36 +0900

    migration 을 좀 더 안전하게 변경

--- pks/notify_model_changed.sh
+++ pks/notify_model_changed.sh
@@ -1,7 +1,8 @@
 sudo -u postgres psql -a -f .script/nosuperuser.sql
-python manage.py makemigrations --noinput
+python manage.py makemigrations
+sleep 3
 sudo -u postgres psql -a -f .script/superuser.sql
-python manage.py migrate --noinput
+python manage.py migrate
+sleep 3
 py.test --create-db
 sudo -u postgres psql -a -f .script/nosuperuser.sql
-
