43f62cfb1c109c26a31196df34b4b4a5057eea75
gulby <gulby@maukistudio.com>
2016-05-23 19:16:53 +0900

    OPTIONS, 부하 문제 없도록 수정

---
+++ pks/pks/config.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework.metadata import BaseMetadata
+
+
+class MinimalMetadata(BaseMetadata):
+    """
+    Don't include field and other information for `OPTIONS` requests.
+    Just return the name and description.
+    """
+    def determine_metadata(self, request, view):
+        return {
+            'name': view.get_view_name(),
+            'description': view.get_view_description()
+        }
--- pks/pks/settings.py
+++ pks/pks/settings.py
@@ -127,6 +127,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_METADATA_CLASS': 'pks.config.MinimalMetadata',
 }
 
 
