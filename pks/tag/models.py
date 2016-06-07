dbb6c72a36051e7f7efabde1871e2695e8f170de
gulby <gulby@maukistudio.com>
2016-06-07 13:32:15 +0900

    TagMatrix 기본 구현

--- pks/tag/models.py
+++ pks/tag/models.py
@@ -4,15 +4,105 @@ from __future__ import unicode_literals
 from django.contrib.gis.db import models
 
 from place.models import UserPlace, Place
+from content.models import TagName
 
 
+# TODO : 튜닝 (캐싱 등)
 class Tag(models.Model):
-    name = models.CharField(max_length=254, blank=True, null=True, default=None)
+    tagName = models.OneToOneField(TagName, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='tag')
     uplaces = models.ManyToManyField(UserPlace, through='UserPlaceTag', related_name='tags')
     places = models.ManyToManyField(Place, through='PlaceTag', related_name='tags')
 
     def __unicode__(self):
-        return self.name
+        return unicode(self.tagName)
+
+    def save(self, *args, **kwargs):
+        if self.id and self.id <= 0:
+            raise NotImplementedError
+        super(Tag, self).save(*args, **kwargs)
+
+    @property
+    def prior(self):
+        return TagMatrix.prior(self)
+
+
+# TODO : 튜닝, 부동소수점 연산 정확성 향상
+class TagMatrix(models.Model):
+    row = models.IntegerField(blank=True, null=True, default=None)
+    col = models.IntegerField(blank=True, null=True, default=None)
+    count = models.IntegerField(blank=True, null=True, default=None)
+
+    class Meta:
+        unique_together = ('row', 'col')
+
+    def save(self, *args, **kwargs):
+        if not (self.row >= 0) or not (self.col >= 0):
+            raise NotImplementedError
+        if self.row < self.col:
+            raise NotImplementedError
+        super(TagMatrix, self).save(*args, **kwargs)
+
+
+    @classmethod
+    def row_col(cls, elem1, elem2):
+        if type(elem1) is Tag:
+            elem1 = elem1.id
+        if type(elem2) is Tag:
+            elem2 = elem2.id
+        return sorted([elem1, elem2], reverse=True)
+
+    @classmethod
+    def inc(cls, row, col):
+        row, col = TagMatrix.row_col(row, col)
+        tm, is_created = cls.objects.get_or_create(row=row, col=col)
+        if is_created:
+            tm.count = 0
+        tm.count += 1
+        tm.save()
+        return tm.count
+
+    @classmethod
+    def inc_places_count(cls):
+        tm, is_created = cls.objects.get_or_create(row=0, col=0)
+        if is_created:
+            tm.count = 0
+        tm.count += 1
+        tm.save()
+        return tm.count
+
+    @classmethod
+    def get(cls, row, col):
+        row, col = TagMatrix.row_col(row, col)
+        try:
+            tm = cls.objects.get(row=row, col=col)
+        except TagMatrix.DoesNotExist:
+            tm = TagMatrix(row=row, col=col, count=0)
+        return tm.count
+
+    @classmethod
+    def places_count(cls):
+        return cls.get(0, 0)
+
+    @classmethod
+    def prior(cls, T):
+        total = TagMatrix.places_count()
+        if total <= 0:
+            return 0.5
+        T_total = TagMatrix.get(T, T)
+        result = (float(T_total)+0.5) / (float(total)+1.0)
+        return result
+
+    @classmethod
+    def likelyhood(cls, D, H):
+        if D == H:
+            return 1.0
+        H_total = TagMatrix.get(H, H)
+        prior_D = TagMatrix.prior(D)
+        if H_total <= 0:
+            return prior_D
+        intersection = TagMatrix.get(D, H)
+        result = (float(intersection)+prior_D) / (float(H_total)+1.0)
+        return result
 
 
 class UserPlaceTag(models.Model):
@@ -20,8 +110,30 @@ class UserPlaceTag(models.Model):
     uplace = models.ForeignKey(UserPlace, on_delete=models.SET_DEFAULT, null=True, default=None, related_name='uptags')
     created = models.BigIntegerField(blank=True, null=True, default=None)
 
+    class Meta:
+        unique_together = ('tag', 'uplace')
+
     def __unicode__(self):
-        return self.tag and self.tag.name or None
+        return self.tag and unicode(self.tag) or None
+
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            raise NotImplementedError
+        if self.uplace and self.uplace.place:
+            self.process_tag()
+        super(UserPlaceTag, self).save(*args, **kwargs)
+
+    def process_tag(self):
+        if self.tag and self.uplace and self.uplace.place:
+            place = self.uplace.place
+            ptag, is_created = PlaceTag.objects.get_or_create(tag=self.tag, place=place)
+            if is_created:
+                tags = place.tags.all()
+                for tag in tags:
+                    TagMatrix.inc(self.tag, tag)
+                if len(tags) == 1:
+                    TagMatrix.inc_places_count()
 
 
 class PlaceTag(models.Model):
@@ -30,5 +142,12 @@ class PlaceTag(models.Model):
     created = models.BigIntegerField(blank=True, null=True, default=None)
     prob = models.FloatField(blank=True, null=True, default=None)
 
+    class Meta:
+        unique_together = ('tag', 'place')
+
     def __unicode__(self):
-        return self.tag and self.tag.name or None
+        return self.tag and unicode(self.tag) or None
+
+    def delete(self, using=None, keep_parents=False):
+        # 삭제하지 말고 prob 을 조정할 것
+        raise NotImplementedError
--- pks/tag/test_models.py
+++ pks/tag/test_models.py
@@ -2,44 +2,97 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+from django.db import IntegrityError
+
 from base.tests import APITestBase
-from tag.models import Tag, UserPlaceTag, PlaceTag
+from tag.models import Tag, TagMatrix, UserPlaceTag, PlaceTag
 from place.models import UserPlace, Place
+from content.models import TagName
 
 
 class TagTest(APITestBase):
 
     def test_string_representation(self):
         tag = Tag()
-        test_name = 'test tag'
-        tag.name = test_name
-        self.assertEqual(unicode(tag), test_name)
+        tagName = TagName.get_from_json({'content': 'test tag'})
+        tag.tagName = tagName
+        self.assertEqual(unicode(tag), unicode(tagName))
 
     def test_save_and_retreive(self):
         tag = Tag()
-        test_name = 'test tag'
-        tag.name = test_name
+        tagName = TagName.get_from_json({'content': 'test tag'})
+        tag.tagName = tagName
         self.assertEqual(Tag.objects.count(), 0)
         tag.save()
         self.assertEqual(Tag.objects.count(), 1)
         saved = Tag.objects.first()
         self.assertEqual(saved, tag)
 
-    def test_value_property(self):
+        tag2 = Tag()
+        tag2.tagName = tagName
+        with self.assertRaises(IntegrityError):
+            tag2.save()
+
+    def test_tagName_property(self):
         tag = Tag()
-        test_name = 'test tag'
-        tag.name = test_name
+        tagName = TagName.get_from_json({'content': 'test tag'})
+        tag.tagName = tagName
         tag.save()
         saved = Tag.objects.first()
-        self.assertEqual(tag.name, test_name)
-        self.assertEqual(saved.name, test_name)
+        self.assertEqual(tag.tagName, tagName)
+        self.assertEqual(saved.tagName, tagName)
+
+    def test_prior_property(self):
+        place1 = Place.objects.create()
+        uplace1 = UserPlace.objects.create(place=place1)
+        self.assertEqual(Place.objects.count(), 1)
+        tag = Tag.objects.create(tagName=TagName.get_from_json({'content': 'test tag'}))
+        self.assertAlmostEqual(tag.prior, (0+0.5)/(0+1.0), delta=0.000001)
+        place2 = Place.objects.create()
+        uplace2 = UserPlace.objects.create(place=place2)
+        self.assertEqual(Place.objects.count(), 2)
+        self.assertAlmostEqual(tag.prior, (0+0.5)/(0+1.0), delta=0.000001)
+
+        self.assertEqual(tag.ptags.count(), 0)
+        uptag1 = UserPlaceTag.objects.create(tag=tag, uplace=uplace1)
+        self.assertEqual(tag.ptags.count(), 1)
+        self.assertAlmostEqual(tag.prior, (1+0.5)/(1+1.0), delta=0.000001)
+        uptag2 = UserPlaceTag.objects.create(tag=tag, uplace=uplace2)
+        self.assertEqual(tag.ptags.count(), 2)
+        self.assertAlmostEqual(tag.prior, (2+0.5)/(2+1.0), delta=0.000001)
+
+
+
+class TagMatrixTest(APITestBase):
+
+    def test_likelyhood_property(self):
+        place1 = Place.objects.create()
+        uplace1 = UserPlace.objects.create(place=place1)
+        self.assertEqual(Place.objects.count(), 1)
+        tag1 = Tag.objects.create(tagName=TagName.get_from_json({'content': 'test tag'}))
+        tag2 = Tag.objects.create(tagName=TagName.get_from_json({'content': 'other tag'}))
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag2, tag1), (0 + (0+0.5)/(0+1.0))/(0+1), delta=0.000001)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag1, tag2), (0 + (0+0.5)/(0+1.0))/(0+1), delta=0.000001)
+        place2 = Place.objects.create()
+        uplace2 = UserPlace.objects.create(place=place2)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag2, tag1), (0 + (0+0.5)/(0+1.0))/(0+1), delta=0.000001)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag1, tag2), (0 + (0+0.5)/(0+1.0))/(0+1), delta=0.000001)
+        uptag1 = UserPlaceTag.objects.create(tag=tag1, uplace=uplace1)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag2, tag1), (0 + (0+0.5)/(1+1.0))/(1+1), delta=0.000001)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag1, tag2), (0 + (1+0.5)/(1+1.0))/(0+1), delta=0.000001)
+        uptag2 = UserPlaceTag.objects.create(tag=tag2, uplace=uplace2)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag2, tag1), (0 + (1+0.5)/(2+1.0))/(1+1), delta=0.000001)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag1, tag2), (0 + (1+0.5)/(2+1.0))/(1+1), delta=0.000001)
+        uptag11 = UserPlaceTag.objects.create(tag=tag1, uplace=uplace2)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag2, tag1), (1 + (1+0.5)/(2+1.0))/(2+1), delta=0.000001)
+        self.assertAlmostEqual(TagMatrix.likelyhood(tag1, tag2), (1 + (2+0.5)/(2+1.0))/(1+1), delta=0.000001)
 
 
 class UserPlaceTagTest(APITestBase):
 
     def setUp(self):
         super(UserPlaceTagTest, self).setUp()
-        self.tag = Tag.objects.create(name='test tag')
+        self.tag = Tag.objects.create(tagName=TagName.get_from_json({'content': 'test tag'}))
         self.uplace = UserPlace.objects.create()
 
     def test_string_representation(self):
@@ -58,26 +111,38 @@ class UserPlaceTagTest(APITestBase):
         saved = UserPlaceTag.objects.first()
         self.assertEqual(saved, uptag)
 
+        uptag2 = UserPlaceTag()
+        uptag2.tag = self.tag
+        uptag2.uplace = self.uplace
+        with self.assertRaises(IntegrityError):
+            uptag2.save()
+
 
 class PlaceTagTest(APITestBase):
 
     def setUp(self):
         super(PlaceTagTest, self).setUp()
-        self.tag = Tag.objects.create(name='test tag')
+        self.tag = Tag.objects.create(tagName=TagName.get_from_json({'content': 'test tag'}))
         self.place = Place.objects.create()
 
     def test_string_representation(self):
         ptag = PlaceTag()
         ptag.tag = self.tag
-        ptag.uplace = self.place
+        ptag.place = self.place
         self.assertEqual(unicode(ptag), unicode(self.tag))
 
     def test_save_and_retreive(self):
         ptag = PlaceTag()
         ptag.tag = self.tag
-        ptag.uplace = self.place
+        ptag.place = self.place
         self.assertEqual(PlaceTag.objects.count(), 0)
         ptag.save()
         self.assertEqual(PlaceTag.objects.count(), 1)
         saved = PlaceTag.objects.first()
         self.assertEqual(saved, ptag)
+
+        ptag2 = PlaceTag()
+        ptag2.tag = self.tag
+        ptag2.place = self.place
+        with self.assertRaises(IntegrityError):
+            ptag2.save()
