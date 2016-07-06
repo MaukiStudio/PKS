c4e45174ae1517ae67055124071747a91930e36b
gulby <gulby@maukistudio.com>
2016-07-06 18:39:36 +0900

    Celery 안되는 문제 해결 : DJANGO_SETTINGS_MODULE 환경변수 세팅해야 함

--- pks/pks/tasks.py
+++ pks/pks/tasks.py
@@ -5,6 +5,9 @@ from __future__ import absolute_import
 from pks.celery import app
 from time import sleep
 
+# Test for DJANGO_SETTINGS_MODULE
+from image.models import Image
+
 
 @app.task
 def for_unit_test(x, y):
