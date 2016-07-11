f78c0b4178a7ba3df3b9010edf7d3432d6b30ed8
gulby <gulby@maukistudio.com>
2016-07-12 01:36:03 +0900

    deploy script 버그 수정

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -29,7 +29,7 @@ sudo mkdir /etc/redis
 sudo mkdir /var/redis
 sudo cp utils/redis_init_script /etc/init.d/redis_6379
 sudo mkdir /var/redis/6379
-sudo ../6379.conf /etc/redis/6379.conf
+sudo cp ../6379.conf /etc/redis/6379.conf
 sudo update-rc.d redis_6379 defaults
 sudo /etc/init.d/redis_6379 start
 
