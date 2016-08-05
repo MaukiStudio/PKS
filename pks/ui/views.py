3af2358083204e18e89f802c26f727d7bc35004b
gulby <gulby@maukistudio.com>
2016-08-05 21:25:55 +0900

    실섭에서 발생한, UserPlace.aid 의 vd_id 값이 0인 문제에 대한 1차 예외 처리 (아직 정확한 원인이 파악되지 않음)

--- pks/ui/views.py
+++ pks/ui/views.py
@@ -23,19 +23,18 @@ def vd_login_for_browser(request):
     # 브라우저 로그인은 VD 로그인만 한다
     # API 호출은 User 로그인까지 되어야 가능하도록 해서 권한 관리를 확실히 분리
     # 그리고 브라우저 로그인에서는 token 발급 및 저장은 없도록 해서 보안 관리를 한다
-    vd_id = VD_SESSION_KEY in request.session and request.session[VD_SESSION_KEY] or None
     vd = None
+    vd_id = VD_SESSION_KEY in request.session and request.session[VD_SESSION_KEY] or None
     if vd_id:
-        try:
-            vd = VD.objects.get(id=vd_id)
-        except VD.DoesNotExist:
-            vd = None
-    if not vd:
+        vd = VD.objects.get(id=vd_id)
+    else:
         user_agent = 'HTTP_USER_AGENT' in request.META and request.META['HTTP_USER_AGENT'][:254] or None
         language = 'HTTP_ACCEPT_LANGUAGE' in request.META and request.META['HTTP_ACCEPT_LANGUAGE'][:2] or None
         vd = VD.objects.create(deviceTypeName='BROWSER', deviceName=user_agent, language=language)
         vd_id = vd.id
         request.session[VD_SESSION_KEY] = vd_id
+    if not vd or not vd_id:
+        raise NotImplementedError
     return vd
 
 
