71619bf688bd39d9859ff75209ae87692a139a28
gulby <gulby@maukistudio.com>
2016-08-29 15:31:57 +0900

    reset_db.sh, 추가된 app 반영

--- pks/reset_db.sh
+++ pks/reset_db.sh
@@ -1,12 +1,16 @@
 sudo -u postgres psql -a -f .script/nosuperuser.sql
 sudo rm -rf ../media
 rm account/migrations/0*
+rm admin2/migrations/0*
 rm base/migrations/0*
 rm content/migrations/0*
 rm functional_tests/migrations/0*
 rm image/migrations/0*
+rm importer/migrations/0*
 rm pks/migrations/0*
 rm place/migrations/0*
+rm tag/migrations/0*
+rm ui/migrations/0*
 rm url/migrations/0*
 sudo -u postgres psql -a -f .script/superuser.sql
 python manage.py reset_db
